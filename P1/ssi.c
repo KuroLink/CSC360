acbc09611cb3972b800d5d58838533e4873d6f9b
KuroLink <cyang11@uvic.ca>
2019-10-01 12:09:58 -0700

    First commit

---
+++ P1/ssi.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<string.h>
+
+int main()
+{
+	char *name;
+       	name=(char *)malloc(20*sizeof(char));
+	cuserid(name);
+	char hostname[20];
+	int rc = gethostname(hostname, sizeof(hostname));
+	char cwd[50];
+	getcwd(cwd, sizeof(cwd));
+	//printf("SSI: %s@%s %s > ", name, hostname, cwd);
+	while(1){
+		printf("SSI: %s@%s %s > ", name, hostname, cwd);
+		char str[100];
+		fgets(str, 25, stdin);
+		//printf("input is %s\n", str);
+		//char* token = strtok(str, " \n");
+		char* args[100];
+		//printf("first token is %s\n", token);
+		//printf("argv array is %d", argv[0]);
+		args[0] = strtok(str, " \n");
+		int i=0;
+		while(args[i]!=NULL){
+			args[i+1]=strtok(NULL, " \n");
+			i++;
+		}
+		//printf("First part of string is %s\n", args[0]);
+		//execvp(args[0], args);
+		//(strcmp(args[0], "exit")==0){
+		//	exit(0);
+		//}
+		else{
+		pid_t pid;
+		int status;
+		if((pid = fork()) <0) {
+			printf("ERROR\n");
+			exit(1);
+		}
+		else if (pid == 0) {
+			
+			if(strcmp(args[0], "exit")==0){
+				exit(1);
+			}	
+				printf("My process ID : %d\n", getpid());
+			
+			if(strcmp(args[0], "cd") ==0){
+				printf("inside cd function and %s\n", args[1]);
+				if(strcmp(args[1], "..") ==0){
+					chdir("..");
+					getcwd(cwd, sizeof(cwd));
+					//printf("SSI: %s@%s %s > ", name, hostname, cwd);
+				}
+				else if(strcmp(args[1], "~")==0 || strcmp(args[1], "")==0){
+					
+					chdir(getenv("HOME"));
+					getcwd(cwd, sizeof(cwd));
+					//printf("SSI: %s@%s %s > ", name, hostname, cwd);
+				}
+				
+			}	
+			else if(execvp(args[0], args) < 0) {
+				printf("ERROR exe\n");
+				exit(1);
+				}
+			}
+		
+		else {
+			while(wait(&status) != pid);
+			}
+		}
+		//execvp(args[0], args);
+		memset(args, 0, 50);	
+	}		
+}
